596aae66a9cf0afc289512b77bd3c778b1a4b959
yashmakk <mymittal777@gmail.com>
2016-05-13 04:02:23 +0530

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -20,4 +20,5 @@ image_url=url+s
 res = requests.get(image_url)
 with open(os.path.join('Bing',cd+'.jpg'),'wb') as file:
     file.write(res.content) 
-os.system('gsettings set org.gnome.desktop.background picture-uri file:///home/makpish/Desktop/Bing/'+cd+'.jpg')
+os.system('gsettings set org.gnome.desktop.background picture-uri file://'+os.getcwd()+'/Bing/'+cd+'.jpg')
+os.system('notify-send "Wallpaper Updated" "Thank you for using"')
